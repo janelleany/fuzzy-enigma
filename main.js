d5a606afee1c6ec49ebca021809391b0f7024e7d
janelleany <janellealexander12@gmail.com>
2018-08-27 17:27:14 -0400

    small changes

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// variables: QUERY & NAME THE DOM ELEMENTS WE NEED TO WORK WITH
+let menuIcon = document.querySelector('.mobile-nav-button');
+let sidecarNav = document.querySelector('.sidecar-nav');
+
+
+// function: TOGGLES EXISTING DOM ELEMENT'S CLASS
+let toggle = (element, className) => {
+  element.classList.toggle(className);
+}
+
+// function: CLICK ON MENU ICON EVENT HANDLER
+let clickMenu = (event) => {
+  toggle(sidecarNav, 'display-none');
+}
+
+menuIcon.addEventListener('click', clickMenu);
+
 // === HTTP service ===
 const express = require('express');
 const app = express();
